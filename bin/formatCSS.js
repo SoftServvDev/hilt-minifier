c26444c9aebcf637ae55d474642397899fcca4a2
Chase Quinn <chase@softservv.com>
2023-07-26 16:31:40 -0400

    Finished outside of comments in CSS files

--- bin/formatCSS.js
+++ bin/formatCSS.js
@@ -6,11 +6,26 @@ class CSSMinifier {
         var data = util.format(dataRaw)
         var dataArray = data.replace((/  |\r\n|\n|\r/gm),"").split("")
         var splitName = filename.split("")
+        function removeSpaceBeforeRule(){
+            for(var i = 0; i < dataArray.length - 1; i++){
+                var x = i - 1
+                if(dataArray[i] == "{"){
+                    if(i > 1){
+                        if(dataArray[x] == " "){
+                            dataArray.splice(x, 1)
+                        }
+                    }
+                }
+            }
+        }
+        removeSpaceBeforeRule()
         for(let i=0; i<4; i++){
             splitName.pop()
         }
         fs.writeFile("./" + splitName.join("") + ".minified.css", dataArray.join(""), err => {
-            console.log(err)
+            if(err){
+                console.log(err)
+            }
         })
     }
 }
